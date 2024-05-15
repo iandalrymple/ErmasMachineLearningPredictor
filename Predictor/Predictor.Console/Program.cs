a7ca7487a3e660a5d02c3ee6bbfcc157247f5f85
IanD <iandalrymple6@gmail.com>
2024-05-15 19:29:51 -0400

    Work in progress on composition root

--- Predictor/Predictor.Console/Program.cs
+++ Predictor/Predictor.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// Get the weather at noon on the current day. 
+﻿using Microsoft.Extensions.Configuration;
+using Predictor.RetrieveOwmWeather.Implementations;
+using RestSharp;
+using Serilog;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("testSettings.json")
+    .Build();
+
+// NEED TO use serilog and cast to MS ILogger
+var logger = new LoggerConfiguration()
+    .WriteTo.File("AppLogs.log", rollOnFileSizeLimit: true, fileSizeLimitBytes: 1048576)
+    .WriteTo.Console()
+    .CreateLogger();
+
+// Create the retriever.
+var rco = new RestClientOptions(config["BaseWeatherUri"]!);
+var retrieveObj = new RetrieveWeather(new RestClient(rco), new RestRequest());
+var loggingRetriever = new LoggingDecoratorRetrieveWeather(retrieveObj, logger);
 
