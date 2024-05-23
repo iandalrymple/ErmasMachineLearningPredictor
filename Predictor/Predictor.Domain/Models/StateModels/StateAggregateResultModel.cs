4cb38f264c927b59a0a5c517d3e3f75d9b582268
IanD <iandalrymple6@gmail.com>
2024-05-23 07:45:19 -0400

    Building out aggergation result model

--- Predictor/Predictor.Domain/Models/StateModels/StateAggregateResultModel.cs
+++ Predictor/Predictor.Domain/Models/StateModels/StateAggregateResultModel.cs
@@ -2,4 +2,53 @@
 
 public class StateAggregateResultModel
 {
+    public decimal Sales_Three_Pm { get; set; }
+
+    public double TempNoon { get; set; }
+    public double FeelsLikeNoon { get; set; }
+    public double PressureNoon { get; set; }
+    public double HumidityNoon { get; set; }
+    public double DewPointNoon { get; set; }
+    public double UviNoon { get; set; }
+    public double CloudsNoon { get; set; }
+    public double VisibilityNoon { get; set; }
+    public double WindSpeedNoon { get; set; }
+    public double WindGustNoon { get; set; }
+    public double WindDegNoon { get; set; }
+
+    public double TempThree { get; set; }
+    public double FeelsLikeThree { get; set; }
+    public double PressureThree { get; set; }
+    public double HumidityThree { get; set; }
+    public double DewPointThree { get; set; }
+    public double UviThree { get; set; }
+    public double CloudsThree { get; set; }
+    public double VisibilityThree { get; set; }
+    public double WindSpeedThree { get; set; }
+    public double WindGustThree { get; set; }
+    public double WindDegThree { get; set; }
+
+    public double TempSix { get; set; }
+    public double FeelsLikeSix { get; set; }
+    public double PressureSix { get; set; }
+    public double HumiditySix { get; set; }
+    public double DewPointSix { get; set; }
+    public double UviSix { get; set; }
+    public double CloudsSix { get; set; }
+    public double VisibilitySix { get; set; }
+    public double WindSpeedSix { get; set; }
+    public double WindGustSix { get; set; }
+    public double WindDegSix { get; set; }
+
+    public double TempNine { get; set; }
+    public double FeelsLikeNine { get; set; }
+    public double PressureNine { get; set; }
+    public double HumidityNine { get; set; }
+    public double DewPointNine { get; set; }
+    public double UviNine { get; set; }
+    public double CloudsNine { get; set; }
+    public double VisibilityNine { get; set; }
+    public double WindSpeedNine { get; set; }
+    public double WindGustNine { get; set; }
+    public double WindDegNine { get; set; }
 }
