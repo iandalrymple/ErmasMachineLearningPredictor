4891cae96e66c864a0ad071a8e53d048b6e68691
IanD <iandalrymple6@gmail.com>
2024-05-24 07:54:02 -0400

    Retrieve sales from sql server in progress.

--- Predictor/Predictor.RetrieveSalesSqlServer/Implementations/RetrieveSales.cs
+++ Predictor/Predictor.RetrieveSalesSqlServer/Implementations/RetrieveSales.cs
@@ -1,12 +1,20 @@
 ﻿using Predictor.Domain.Abstractions;
+using System.Data.SqlClient;
 
-namespace Predictor.RetrieveSalesSqlServer.Implementations
+namespace Predictor.RetrieveSalesSqlServer.Implementations;
+
+public class RetrieveSales : IRetrieveSales
 {
-    public class RetrieveSales : IRetrieveSales
+    private readonly string _connectionString;
+
+    public RetrieveSales(string connectionString)
     {
-        public Task<decimal> Retrieve(DateTime dateTime)
-        {
-            throw new NotImplementedException();
-        }
+        _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+    }
+
+    public async Task<decimal> Retrieve(DateTime dateTime)
+    {
+        await using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
     }
 }
---
+++ Predictor/Predictor.Testing/RetrieveSalesSqlServer/TestRetrieveSalesSqlServer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Predictor.Testing.RetrieveSalesSqlServer;
+
+public class TestRetrieveSalesSqlServer
+{
+    private readonly IConfiguration _configuration;
+
+    private readonly DateTime _dateToRetrieve;
+
+    public TestRetrieveSalesSqlServer()
+    {
+        _configuration = new ConfigurationBuilder()
+            .AddJsonFile("testSettings.json")
+            .Build();
+
+        _dateToRetrieve = DateTime.SpecifyKind(new DateTime(year: 2023, month: 4, day: 19), DateTimeKind.Utc);
+    }
+
+    [Fact]
+    public async Task TestRetrieve()
+    {
+        // Arrange
+        var sut = new Predictor.RetrieveSalesSqlServer.Implementations.RetrieveSales(_configuration["ConnectionStringSqlExpressOne"]!);
+
+        // Act
+        var result = await sut.Retrieve(_dateToRetrieve);
+
+        // Assert
+        Assert.True(result > 0);
+    }
+}
