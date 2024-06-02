3a444c659250e0df019088d99a26f31850533991
IanD <iandalrymple6@gmail.com>
2024-06-02 09:50:29 -0400

    Shelled in predictor state.

---
+++ Predictor/Predictor.Domain/Implementations/States/StatePredict.cs
@@ -0,0 +1,19 @@
+﻿using Predictor.Domain.Abstractions;
+using Predictor.Domain.Models;
+using Predictor.Domain.System;
+
+namespace Predictor.Domain.Implementations.States;
+
+public class StatePredict : IFsmState
+{
+    public StatePredict()
+    {
+        State = PredictorFsmStates.Predict;
+    }
+
+    public PredictorFsmStates State { get; init; }
+    public Task Execute(FsmStatefulContainer container)
+    {
+        throw new NotImplementedException();
+    }
+}
