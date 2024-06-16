c07de60bb43f741d298d705f41bdd67cfec2ee3d
IanD <iandalrymple6@gmail.com>
2024-06-16 19:11:43 -0400

    Work in progress for email sales retrieval

--- Predictor/Predictor.RetrieveSalesEmail/Implementations/RetrieveSales.cs
+++ Predictor/Predictor.RetrieveSalesEmail/Implementations/RetrieveSales.cs
@@ -1,13 +1,24 @@
-﻿using Predictor.Domain.Abstractions;
+﻿using BasicEmailLibrary.Lib;
+using Predictor.Domain.Abstractions;
 using Predictor.Domain.Models.StateModels;
 
 namespace Predictor.RetrieveSalesEmail.Implementations
 {
     internal class RetrieveSales : IRetrieveSales<StateCurrentSalesResultModel>
     {
+        private readonly BasicEmail _email;
+
+        public RetrieveSales(BasicEmail email)
+        {
+            _email = email;
+        }
+
         public Task<StateCurrentSalesResultModel> Retrieve(DateTime dateTime, string storeName)
         {
-            throw new NotImplementedException();
+            // Get all the emails 
+            var emails = _email.GetAllUnreadEmail();
+
+            // Now we need to fish through them and grab the one from 3 pm. 
         }
     }
 }
