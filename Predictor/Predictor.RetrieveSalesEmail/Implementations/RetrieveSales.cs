b3e24b3dd28908c0cc3e06e367438bcdf96f366d
IanD <iandalrymple6@gmail.com>
2024-06-16 18:50:42 -0400

    Shelled in new project for retrieving sales from email report.

---
+++ Predictor/Predictor.RetrieveSalesEmail/Implementations/RetrieveSales.cs
@@ -0,0 +1,13 @@
+﻿using Predictor.Domain.Abstractions;
+using Predictor.Domain.Models.StateModels;
+
+namespace Predictor.RetrieveSalesEmail.Implementations
+{
+    internal class RetrieveSales : IRetrieveSales<StateCurrentSalesResultModel>
+    {
+        public Task<StateCurrentSalesResultModel> Retrieve(DateTime dateTime, string storeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
