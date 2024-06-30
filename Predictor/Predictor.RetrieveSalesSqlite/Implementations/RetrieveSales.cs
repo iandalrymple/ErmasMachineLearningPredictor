a5c19dcc261cff7ad02ba8cce89df14725da8ee5
IanD <iandalrymple6@gmail.com>
2024-06-30 10:28:59 -0400

    Work in progress on sqlite retriever

---
+++ Predictor/Predictor.RetrieveSalesSqlite/Implementations/RetrieveSales.cs
@@ -0,0 +1,20 @@
+﻿using Predictor.Domain.Abstractions;
+using Predictor.Domain.Models.StateModels;
+
+namespace Predictor.RetrieveSalesSqlite.Implementations
+{
+    public class RetrieveSales : IRetrieveSales<StateCurrentSalesResultModel?>
+    {
+        private readonly string _connectionString;
+
+        public RetrieveSales(string connectionString)
+        {
+            _connectionString = connectionString;   
+        }
+
+        public Task<StateCurrentSalesResultModel?> Retrieve(DateTime dateTime, string storeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Predictor/Predictor.Testing/RetrieveSalesSqlite/TestRetrieveSalesSqlite.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Predictor.Console.Composition;
+using Predictor.Testing.Supporting;
+
+namespace Predictor.Testing.RetrieveSalesSqlite;
+
+public class TestRetrieveSalesSqlite
+{
+    private readonly IConfiguration _configuration = ConfigurationSingleton.Instance;
+
+    [Theory]
+    [InlineData(2024, 6, 19, 1127.85, 660, 0)]
+    [InlineData(2024, 6, 20, 858.53, 660, 0)]
+    public async Task TestRetrieve(int year, int month, int day, decimal salesAtThree, uint firstOrderTime, uint lastOrderTime)
+    {
+        // Arrange
+        var sut = new Predictor.RetrieveSalesSqlite.Implementations.RetrieveSales("basicEmail");
+
+        // Act
+        var dateTime = new DateTime(year: year, month: month, day: day);
+        var result = await sut.Retrieve(dateTime, "Utica");
+
+        // Assert
+        Assert.Equal(salesAtThree, result.SalesAtThree, 0);
+        Assert.Equal(firstOrderTime, result.FirstOrderMinutesInDay);
+        Assert.Equal(lastOrderTime, result.LastOrderMinutesInDay);
+    }
+}
