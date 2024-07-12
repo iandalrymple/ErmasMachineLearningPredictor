737b002d27149c4583ff5c93037d57b864ee7b64
IanD <iandalrymple6@gmail.com>
2024-07-12 19:05:16 -0400

    Framed in the cache for weather

---
+++ Predictor/Predictor.RetrieveOwmWeatherSqlite/Implementations/RetrieveWeather.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SQLite;
+using Predictor.Domain.Abstractions;
+using Predictor.Domain.Models;
+
+namespace Predictor.RetrieveOwmWeatherSqlite.Implementations
+{
+    public class RetrieveWeather : IRetrieveWeather
+    {
+        private readonly string _connectionString;
+        private readonly SQLiteConnection _connection;
+
+        public RetrieveWeather(string connectionString)
+        {
+            _connectionString = connectionString;
+            _connection = new SQLiteConnection(connectionString);
+        }
+
+        public async Task<WeatherSourceModel> Retrieve(WeatherRetrieveParamModel inParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
